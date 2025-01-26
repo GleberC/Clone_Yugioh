f2e9cd33d3526512e6158c29d5c8f8bc8242bb01
Gleber <gleber10@yahoo.com.br>
2025-01-26 19:21:29 -0300

    tirando os comentarios

--- src/js/index.js
+++ src/js/index.js
@@ -5,15 +5,7 @@ const btnVoltar = document.getElementById("btn-voltar");
 const cartoes = document.querySelectorAll(".cartao");
 let cartaoAtual = 0;
 
-/*
 
-OBJETIVO 1 - quando clicarmos na seta de avançar temos que mostrar o proximo 
-cartao da lista
-  - passo 1 - dar um jeito de pegar o elemento HTML da seta avancar
-  - passo 2 - dar um jeito de identificar o clique do usuário na seta avançar
-     - passo 3 - fazer aparecer o próximo cartão da lista 
-    - passo 4 - buscar o cartão que esta selecionado e esconder
-    */
 
     btnAvancar.addEventListener("click", function () {
         if (cartaoAtual === cartoes.length - 1) return;
@@ -23,18 +15,7 @@ cartao da lista
         cartaoAtual++;
         mostrarCartao(cartaoAtual);
       });
-      
-
-
-
-    /*
-    OBJETIVO 2 - quando clicarmos na seta de voltar temos que mostrar o cartão 
-anterior da lista
-  - passo 1 - dar um jeito de pegar o elemento HTML da seta voltar
-  - passo 2 - dar um jeito de identificar o clique do usuário na seta voltar
-  - passo 3 - fazer aparecer o cartão anterior da lista
-    - passo 4 - buscar o cartão que esta selecionado e esconder
-*/
+   
 
 btnVoltar.addEventListener("click", function () {
     if (cartaoAtual === 0) return;
